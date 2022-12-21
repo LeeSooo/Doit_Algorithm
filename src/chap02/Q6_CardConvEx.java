4d7cd38fb13c3aafff6aec78e233cf40a8abc94e
LeeSooo <dltn990930@naver.com>
2022-12-21 13:18:13 +0900

    second week (2022-12-21 이수)

--- src/chap02/Q6_CardConvEx.java
+++ src/chap02/Q6_CardConvEx.java
@@ -5,34 +5,42 @@ import java.util.Scanner;
 public class Q6_CardConvEx {
 	//--- 정숫값 x를 r 진수로 변환하여 배열 d에 아랫자리부터 저장하고 자릿수를 반환 ---//
 	static int cardConvEx(int x, int r, char[] d) {
-		int n = ((Integer)x).toString().length();		// 변환 전의 자릿수
-		int digits = 0;									// 변환 후의 자릿수
+		int n = ((Integer)x).toString().length();		// 변환 전의 자릿수 => 자리 수 만큼 '_' 이 기호를 채우기 위한 존재. 
+														// => 문자열로 변환 후 문자열 길이를 체크!
+		
+		int digits = 0;									// 변환 후의 자릿수 => 진수 변환을 하기 위해 저장하는 변수.
 		String dchar = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-		System.out.printf(String.format("%%2d | %%%dd\n", n), r, x);
+		System.out.println(r + " | " +  x);
 		do {
-			System.out.print("   +");
+			System.out.print("  +");
+			
+			// 문자열 자리 수 만큼 '_' 입력
 			for (int i = 0; i < n + 2; i++)
 				System.out.print('-');
+			
 			System.out.println();
 
 			// 몫이 0 이 아닐 경우.
-			if (x / r != 0)					//   진수	  | 몫		 ... 나머지
-				System.out.printf(String.format("%%2d | %%%dd    ... %%d\n", n),
-												r, x / r, x % r);	// 진수, 몫, 나머지
+			if (x / r != 0)	
+				System.out.println(r + " | " + x / r + "    ... " + x % r);	// 진수, 몫, 나머지
 			
-			else							//	 진수	  | 몫	   ... 나머지
-				System.out.printf(String.format("     %%%dd    ... %%d\n", n),
-												x / r, x % r);		// 진수, 몫, 나머지
+			// 몫이 0 인 경우.
+			else
+				System.out.println("    " + x / r + "    ... " + x % r);		// 진수, 몫, 나머지
+			
 			d[digits++] = dchar.charAt(x % r);   // r 로 나눈 나머지를 저장
 			x /= r;
+			
 		} while (x != 0);
 
+		// 배열 d를 역순으로 정렬
 		for (int i = 0; i < digits/ 2; i++) {   // d[0] ~ d[digits-1]
-			char temp = d[i];                  // 을 역순으로 정렬
+			char temp = d[i];
 			d[i] = d[digits - i - 1];
 			d[digits - i - 1] = temp;
 		}
+		
 		return digits;
 	}
 
