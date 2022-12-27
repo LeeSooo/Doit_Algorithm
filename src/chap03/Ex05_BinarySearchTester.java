37d12c07f1d229a902d5189af5e30a8d866d4dfa
LeeSooo <dltn990930@naver.com>
2022-12-27 13:20:48 +0900

    third week (2022-12-27 이수)

--- src/chap03/Ex05_BinarySearchTester.java
+++ src/chap03/Ex05_BinarySearchTester.java
@@ -4,7 +4,7 @@ package chap03;
 import java.util.Arrays;
 import java.util.Scanner;
 
-class BinarySearchTester {
+class Ex05_BinarySearchTester {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
